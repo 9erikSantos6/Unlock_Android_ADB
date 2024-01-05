8db5d2e33d3199ca068a383235a5996698185c4d
Erik Santos <erik2018321654@gmail.com>
2024-01-05 15:53:49 -0300

    Update crack-adb-unlock.sh

--- crack-adb-unlock.sh
+++ crack-adb-unlock.sh
@@ -5,5 +5,5 @@ adb shell mount data && sleep "1" &&
 adb shell umount system && sleep "1" &&
 adb shell mount system && sleep "1" &&
 adb push ./persist.sys.usb.config /data/property && sleep "1" &&
-adb shell echo "\n# Adcicionado pelo script:\npersist.service.adb.enable=1\npersist.service.debuggable=1\npersist.sys.usb.config=mtp,adb" >> /system/build.prop
+adb shell echo "\n# Adcicionado pelo Crack_ADB_Unlock:\npersist.service.adb.enable=1\npersist.service.debuggable=1\npersist.sys.usb.config=mtp,adb" >> /system/build.prop
 # adb push ./build.prop /system/ 
