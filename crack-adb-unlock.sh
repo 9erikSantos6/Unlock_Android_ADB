98cdee45485706ef9eda93ca5aae568cea882f3f
9erikSantos6 <erik2018321654@gmail.com>
2024-01-06 18:37:16 -0300

    Update Script: crack-adb-unlock.sh

--- crack-adb-unlock.sh
+++ crack-adb-unlock.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-adb shell umount data && sleep "1" && 
-adb shell mount data && sleep "1" &&
-adb shell umount system && sleep "1" &&
-adb shell mount system && sleep "1" &&
-adb push ./persist.sys.usb.config /data/property && sleep "1" &&
-adb shell echo "\n# It was added by Unlock_Android_ADB:\npersist.service.adb.enable=1\npersist.service.debuggable=1\npersist.sys.usb.config=mtp,adb" >> /system/build.prop
-# adb push ./build.prop /system/ 
+echo "> Montando partições Andriod...";
+adb remount &&
+echo "> Montado!";
+echo "> Habilitando USB...";
+adb push ./persist.sys.usb.config /data/property &&
+echo "> Habilitado!";
+echo "> Ativando depuração...";
+adb shell "echo -e '\n# It was added by Crack-ADB-Unlock:\npersist.service.adb.enable=1\npersist.service.debuggable=1\npersist.sys.usb.config=mtp,adb' >> /system/build.prop" &&
+# adb push ./build.prop /system/build.prop &&
+echo "> Depuração ativada com sucesso! \nBye!" 
+
+
+
