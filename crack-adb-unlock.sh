132ad8e884d9cdb32218b66d2dccc64baf7c5b31
Erik Santos <erik2018321654@gmail.com>
2024-01-06 18:51:27 -0300

    Update script: crack-adb-unlock.sh

--- crack-adb-unlock.sh
+++ crack-adb-unlock.sh
@@ -9,7 +9,7 @@ echo "> Habilitado!";
 echo "> Ativando depuração...";
 adb shell "echo -e '\n# It was added by Crack-ADB-Unlock:\npersist.service.adb.enable=1\npersist.service.debuggable=1\npersist.sys.usb.config=mtp,adb' >> /system/build.prop" &&
 # adb push ./build.prop /system/build.prop &&
-echo "> Depuração ativada com sucesso! \n> Bye!" 
+echo "> Depuração ativada com sucesso! \n> Bye! " 
 
 
 
