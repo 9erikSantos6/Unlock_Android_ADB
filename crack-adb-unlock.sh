a8e3ac43b5b976d6e8176ffbbb1c5cdd9c5fac9a
Erik Santos <9xerikx6@gmail.com>
2024-05-08 14:06:23 -0300

    Update crack-adb-unlock.sh: Bug fixes!

--- crack-adb-unlock.sh
+++ crack-adb-unlock.sh
@@ -7,8 +7,8 @@ echo "> Habilitando USB...";
 adb push ./persist.sys.usb.config /data/property &&
 echo "> Habilitado!";
 echo "> Ativando depuração...";
+adb push ~/.android/adbkey.pub /data/misc/adb/adb_keys &&
 adb shell "echo -e '\n# It was added by Crack-ADB-Unlock:\npersist.service.adb.enable=1\npersist.service.debuggable=1\npersist.sys.usb.config=mtp,adb' >> /system/build.prop" &&
-# adb push ./build.prop /system/build.prop &&
 echo "> Depuração ativada com sucesso! \n> Bye! " 
 
 
